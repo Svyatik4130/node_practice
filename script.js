454f05ff873abd58e192ab31e9bfba7626ccdbf6
Святослав Леонов <appletrollface@gmail.com>
2020-08-17 19:50:31 +0300

    3rd commit with express

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const circle = require('./circles.js');
+const express = require('express');
+const app = exress();
 console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
+app.listen(4555, () => {
+    console.log("server started");
+});
